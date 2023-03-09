10fc7ae8db0faf46c4fc3c5916faaf064a431241
Provd5 <provd12345@gmail.com>
2023-03-09 15:59:01 +0100

    added title

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -3,11 +3,13 @@ import { Poppins } from "next/font/google";
 import "styles/global.scss";
 
 import DefaultHeadTags from "./DefaultHeadTags";
+
 const poppinsFont = Poppins({
   weight: ["300", "400", "600", "700"],
   subsets: ["latin", "latin-ext"],
   display: "swap",
 });
+
 const modeInitializerScript = `(function () {document.documentElement.className = window.localStorage.getItem("mode") || window.matchMedia("(prefers-color-scheme: dark)").matches && "dark" || "light";})();`;
 
 export default function RootLayout({
