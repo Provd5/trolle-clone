8ce403964b272d8e1b8e72636421bdacecb2430c
Provd5 <102181040+Provd5@users.noreply.github.com>
2023-03-09 18:46:31 +0100

    dodanie faviconu, (#6)
    dodanie dark mode i kolorow

--- front/app/DefaultHeadTags.tsx
+++ front/app/DefaultHeadTags.tsx
@@ -1,11 +1,34 @@
 export default function DefaultHeadTags() {
   return (
     <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#59c230" />
+      <meta name="msapplication-TileColor" content="#df8a57" />
+      <meta name="theme-color" content="#ffffff" />
+
       <meta charSet="UTF-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <title>Project T.U.T.E.L</title>
+      <title>Projekt T.U.T.E.L</title>
+      <meta name="description" content="Projekt T.U.T.E.L" />
     </>
   );
 }
