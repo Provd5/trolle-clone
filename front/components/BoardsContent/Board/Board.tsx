efdcf540b6d81d9d4aee638b0694d7333020887e
Provd5 <provd12345@gmail.com>
2023-03-15 23:39:33 +0100

    Drag and drop

--- front/components/BoardsContent/Board/Board.tsx
+++ front/components/BoardsContent/Board/Board.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { DummyData } from "data/DummyData";
 
 import { BoardTypes, ColumnTypes } from "types/ContentDataStructure";
@@ -13,6 +14,11 @@ export default function Board() {
   const [board, setBoard] = useState<BoardTypes>();
   const [columns, setColumns] = useState<ColumnTypes[]>([]);
 
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) return;
+    const { destination, source } = result;
+  };
+
   useEffect(() => {
     const boardData = DummyData.boards.find((item) => item.id === "board-1");
     if (boardData) {
@@ -22,29 +28,29 @@ export default function Board() {
   }, []);
 
   return (
-    <div className="gradient-dir-1 relative flex h-full flex-col">
-      {!board ? (
-        <div className="flex w-full items-center justify-center p-5">
-          Ładowanie tablicy...
-        </div>
-      ) : (
-        <>
-          <div className="flex min-h-[40px] items-center justify-between py-3 px-4">
-            <div>Board title</div>
-            <div>jakas ikona</div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="gradient-dir-1 relative flex h-full flex-col">
+        {!board ? (
+          <div className="flex w-full items-center justify-center p-5">
+            Ładowanie tablicy...
           </div>
-          <div className="boardBodyScrollBar relative mb-2 h-full snap-x snap-mandatory overflow-x-auto overflow-y-hidden md:snap-none">
-            <div className="absolute inset-0 flex">
-              <div className="flex gap-2 px-2 pb-2">
+        ) : (
+          <>
+            <div className="flex min-h-[40px] items-center justify-between py-3 px-4">
+              <div>Board title</div>
+              <div>jakas ikona</div>
+            </div>
+            <div className="relative mb-2 h-full">
+              <div className="boardBodyScrollBar absolute inset-0 flex select-none snap-x snap-mandatory gap-2 overflow-x-auto overflow-y-hidden px-2 pb-2 md:snap-none">
                 {columns &&
                   columns.map((column: ColumnTypes) => (
                     <Column column={column} key={column.id} />
                   ))}
               </div>
             </div>
-          </div>
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </div>
+    </DragDropContext>
   );
 }
