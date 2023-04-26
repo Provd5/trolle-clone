739d06fbb3a2d4d2c6045c9ee145cc51a16e1846
Provd5 <102181040+Provd5@users.noreply.github.com>
2023-04-26 18:05:42 +0200

    Delete back/dist directory

--- back/dist/utils/mapOrder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mapOrder = void 0;
-const mapOrder = (array, order) => {
-    array.sort((a, b) => order.indexOf(a.id) - order.indexOf(b.id));
-    return array;
-};
-exports.mapOrder = mapOrder;
